19504c1ee338d15e92b6728e56429bca735ba78d
Dimitry Nagorny <dimitry.nagorny@gmx.de>
2017-11-15 14:08:37 +0100

    hashes finish

--- koans/path_to_enlightenment.rb
+++ koans/path_to_enlightenment.rb
@@ -22,7 +22,7 @@ require 'about_triangle_project'
 require 'about_exceptions'
 require 'about_triangle_project_2'
 require 'about_iteration'
-require 'about_blocks'
+require 'about_blocks' #SL bis hier
 require 'about_sandwich_code'
 require 'about_scoring_project'
 require 'about_classes'
