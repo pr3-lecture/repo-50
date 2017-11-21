c508ab776e7531c14ca773d13dfdfd6ed1bb99c0
Dimitry Nagorny <dimitry.nagorny@gmx.de>
2017-11-21 16:47:53 +0100

    strings 20

--- koans/about_strings.rb
+++ koans/about_strings.rb
@@ -145,9 +145,10 @@ EOS
 
   def test_you_can_get_a_single_character_from_a_string
     string = "Bacon, lettuce and tomato"
-    assert_equal __, string[1]
+    assert_equal "a", string[1]
 
     # Surprised?
+    # Nope
   end
 
   in_ruby_version("1.8") do
