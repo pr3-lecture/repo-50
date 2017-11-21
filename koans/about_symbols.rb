d5fc713e450a3c58e3b86201c90c6d1cfd430c46
Dimitry Nagorny <dimitry.nagorny@gmx.de>
2017-11-21 16:59:39 +0100

    symbol6

--- koans/about_symbols.rb
+++ koans/about_symbols.rb
@@ -64,13 +64,13 @@ class AboutSymbols < Neo::Koan
     symbol = :cats
     string = "It is raining #{symbol} and dogs."
 
-    assert_equal __, string
+    assert_equal "It is raining cats and dogs.", string
   end
 
   def test_symbols_are_not_strings
     symbol = :ruby
-    assert_equal __, symbol.is_a?(String)
-    assert_equal __, symbol.eql?("ruby")
+    assert_equal false, symbol.is_a?(String)
+    assert_equal false, symbol.eql?("ruby")
   end
 
   def test_symbols_do_not_have_string_methods
