afffecf0d840fa4f0ed74b34307b8ad2a42dbde4
Dimitry Nagorny <dimitry.nagorny@gmx.de>
2017-12-20 16:33:12 +0100

    encrypt ready

--- crypter/crypter/crypter/crypto.c
+++ crypter/crypter/crypter/crypto.c
@@ -1,6 +1,60 @@
 #include "crypto.h"
+#include <stdio.h>
+#include <string.h>
+
+int getPositionForChar(char* searchPool, char searchedChar) {
+	int poolLength = strlen(searchPool);
+	for (size_t i = 0; i < poolLength; i++) {
+		if (searchPool[i] == searchedChar) {
+			return i;
+		}
+	}
+	return -1;
+}
+
+int getPositionForMessage(char searchedChar) {
+	int result = getPositionForChar(MESSAGE_CHARACTERS, searchedChar);
+	if (result < 0) {
+		return E_MESSAGE_ILLEGAL_CHAR;
+	}
+	else {
+		return result;
+	}
+}
+
+int getPositionForKey(char searchedChar) {
+	int result = getPositionForChar(KEY_CHARACTERS, searchedChar);
+	if (result < 0) {
+		return E_KEY_ILLEGAL_CHAR;
+	}
+	else {
+		return result;
+	}
+}
+
+char getEncryptedChar(int searchedPosition) {
+	int poolLength = strlen(CYPHER_CHARACTERS);
+	char* pool = CYPHER_CHARACTERS;
+	for (size_t i = 0; i < poolLength; i++) {
+		if (i == searchedPosition) {
+			return pool[i];
+		}
+	}
+	return E_CYPHER_ILLEGAL_CHAR;
+}
+
+char encryptChar(char in, char key) {
+	unsigned int resultInt = getPositionForMessage(in) ^ getPositionForKey(key);
+	return getEncryptedChar(resultInt);
+}
 
 int encrypt(KEY key, const char * input, char * output) {
+	int keyLength = strlen(key.chars);
+	int inputLength = strlen(input);
+	for (size_t i = 0; i < inputLength; i++) {
+		output[i] = encryptChar(input[i], key.chars[i%keyLength]);
+		output[i + 1] = "\0";
+	}
 	return 0;
 }
 
---
+++ crypter/crypter/crypter/test.c
@@ -0,0 +1,48 @@
+#define mu_assert(message, test) do { if (!(test)) return message; } while (0)
+#define mu_run_test(test) do { char *message = test(); tests_run++; \
+                                if (message) return message; } while (0)
+
+#include <stdio.h>
+#include <string.h>
+#include "crypto.h"
+
+int tests_run = 0;
+
+static char* encryptTest() {
+	KEY key = { .type = 1, .chars = "TPE"};
+	char input[6] = "HALLO";
+	char output[6] = "";
+	int result = encrypt(key, input, output);
+	mu_assert("Encryption failed!", output == "TOOXA");
+}
+
+static char* decryptTest() {
+	mu_assert("Decryption failed!", 1 == 1);
+}
+
+static char* factoryTest() {
+	mu_assert("Factory failed!", 1 == 1);
+}
+
+static char* errorTest() {
+	mu_assert("No Error occured, but expected!", 1 == 1);
+}
+
+static char* allTests() {
+	mu_run_test(encryptTest);
+	mu_run_test(decryptTest);
+	mu_run_test(factoryTest);
+	mu_run_test(errorTest);
+	return 0;
+}
+
+int main() {
+	char *result = allTests();
+
+	if (result != 0) printf("%s\n", result);
+	else             printf("ALL TESTS PASSED\n");
+
+	printf("Tests run: %d\n", tests_run);
+
+	return result != 0;
+}
